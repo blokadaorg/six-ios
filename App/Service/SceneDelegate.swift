fadabebc48ec1143cd4539d1e836d8ef35289e33
kar <hi@kar.gs>
2023-12-21 09:42:22 +0100

    make sure fg/bg events are symmetric

--- App/Service/SceneDelegate.swift
+++ App/Service/SceneDelegate.swift
@@ -79,13 +79,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        foreground.onForeground(true)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        
+
         // Close the kb when leaving to bg
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 
@@ -94,6 +93,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         UIApplication.shared.applicationIconBadgeNumber = 0
+        foreground.onForeground(true)
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
